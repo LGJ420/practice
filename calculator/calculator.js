3ba6ec59f7b9b63e08ffb719e1e535ede82bf1bb
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-04-08 00:44:57 +0900

    My_create_btn(text) 추가

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,23 +1,48 @@
-function cal_add(a, b){
-    return a + b;
-}
+// function my_cal_add(a, b){
+//     return a + b;
+// }
 
-function cal_sub(a, b){
-    return a - b;
-}
+// function my_cal_sub(a, b){
+//     return a - b;
+// }
 
-function cal_mul(a, b){
-    return a * b;
-}
+// function my_cal_mul(a, b){
+//     return a * b;
+// }
 
-function cal_div(a, b){
-    return a / b;
-}
+// function my_cal_div(a, b){
+//     return a / b;
+// }
 
-
+let show = "";
 let board = [];
 
-document.getElementById("test1").addEventListener("click", ()=>{
-    board.push("1");
-    document.getElementById("show").innerHTML = board[0];
+function My_create_btn(text){
+    let btn = document.createElement("button");
+    let node = document.createTextNode(text);
+    btn.id = text;
+    btn.appendChild(node);
+    btn.addEventListener("click",()=>{
+        board.push(text);
+        show += text;
+        document.getElementById("show").innerHTML = show;
+    });
+    document.body.appendChild(btn);
+}
+
+for(let i = 0; i<10; i++){
+    My_create_btn(i);
+}
+
+My_create_btn("+");
+My_create_btn("-");
+My_create_btn("*");
+My_create_btn("/");
+
+My_create_btn("=");
+document.getElementById("=").addEventListener("click",()=>{
+    let result = parseInt(show);
+    show = "";
+    document.getElementById("show").innerHTML = result;
 });
+
